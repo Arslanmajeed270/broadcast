f0148e25468a59d8e42c637020566b806b8bfdfd
Arslan Majeed <arslanmajeed270@gmail.com>
2020-09-05 10:57:59 +0500

    AB-9:  completed room listing with specific room

--- amoxt-broadcast-service/src/util/crypto.js
+++ amoxt-broadcast-service/src/util/crypto.js
@@ -11,12 +11,15 @@ exports.encrypt = (text) => {
     let cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+    return `${iv.toString('hex')} ${encrypted.toString('hex')}`
+    // return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
    }
    
 exports.decrypt = (text) => {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    // let iv = Buffer.from(text.iv, 'hex');
+    let iv = Buffer.from(text.split(" ")[0], 'hex');
+    // let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let encryptedText = Buffer.from(text.split(" ")[1], 'hex');
     let decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);

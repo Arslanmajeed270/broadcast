402def20615194c81940289541e836501ac53b7e
Khalid Bashir <bashir@amoxt.com>
2020-09-08 01:04:02 +0500

    AB-33: Console logs

--- config/.jitsi-meet-cfg/prosody/prosody-plugins-custom/mod_amoxt_service.lua
+++ config/.jitsi-meet-cfg/prosody/prosody-plugins-custom/mod_amoxt_service.lua
@@ -15,7 +15,7 @@ end);
 
 -- Extract 'token' param from URL when session is created
 function init_session(event)
-    -- log("info", "init session");
+    log("info", "init session");
     local session, request = event.session, event.request;
     local query = request.url.query;
 
@@ -24,7 +24,7 @@ function init_session(event)
 
 		session.token = query and params.token or nil;
 		session.password = query and params.password or nil;
-		-- log("info", "checking init session: session PASSWORD: %s -> TOKEN: %s -> RoomName: %s", tostring(session.password), tostring(session.token), tostring(params.room));
+		log("info", "checking init session: session PASSWORD: %s -> TOKEN: %s -> RoomName: %s", tostring(session.password), tostring(session.token), tostring(params.room));
 
 		
 		session.previd = query and params.previd or nil;
@@ -36,10 +36,10 @@ function init_session(event)
 		local expTime = 3600;
 		local PRIVATE_KEY = "amoxtsolutions123456789abcdefghi";
 		local raw = params.room.."|"..session.password.."|"..expTime.."|"..PRIVATE_KEY; 
-		-- log("info", "checking i am into verify_token raw: %s", raw);
+		log("info", "checking i am into verify_token raw: %s", raw);
 		local decoded = hex.to(sha256(raw));
- 		-- log("info", "checking decoded: %s", tostring(decoded) );
- 		-- log("info", "checking token: %s", tostring(decoded) );
+ 		log("info", "checking decoded: %s", tostring(decoded) );
+ 		log("info", "checking token: %s", tostring(decoded) );
 	 	if decoded ~= session.token then
  			session.close(session, 'unauthorized');
  		end
